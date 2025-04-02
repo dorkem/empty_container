cb8713f2c61dba43b97664e4c1b291a7ad20a0d0
dorkem <20233144@gs.cwnu.ac.kr>
2025-04-02 15:55:33 +0900

    :recycle: properties = key:value로 되어있던걸 xml형식으로 변환

--- app/src/main/java/Game.java
+++ app/src/main/java/Game.java
@@ -1,31 +1,16 @@
-import game.AppConfig;
-import game.Human;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import game.Human;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Game {
-    public static void main(String[] args) throws IOException {
-        Properties props = new Properties();
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        try (InputStream input = Game.class.getClassLoader().getResourceAsStream("application.properties")) {
-            if (input == null) {
-                throw new FileNotFoundException("application.properties not found in resources!");
-            }
-            props.load(input);
-        }
+        Human player1 = context.getBean("player1", Human.class);
+        Human player2 = context.getBean("player2", Human.class);
 
-        AppConfig config = AppConfig.getInstance();
-
-        String weaponType1 = props.getProperty("weapon1");
-        String weaponType2 = props.getProperty("weapon2");
-
-        Human human1 = config.createHuman(weaponType1, "hoon");
-        Human human2 = config.createHuman(weaponType2, "Bazzi");
-
-        human1.attackHuman(human2);
-        human2.attackHuman(human1);
+        player1.attackHuman(player2);
+        player2.attackHuman(player1);
     }
 }
